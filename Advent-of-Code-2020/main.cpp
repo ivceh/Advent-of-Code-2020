8dce1baf71dc488e88dc51321dd98bb7670f66bc
ivceh <ivan.ceh1234@gmail.com>
2020-12-02 13:33:44 +0100

    Initial Commit

---
+++ Advent-of-Code-2020/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Advent-of-Code-2020
+//
+//  Created by Ivan Čeh on 02.12.2020..
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
